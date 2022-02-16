276056e5afa4ab6a776e767fa79ae9fbd6b59488
Danish Nawaz <danninnawaz5@yahoo.com>
2022-02-16 09:57:36 +0500

    Pong

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+   public Paddle playerPaddle;
+   public Paddle computerPaddle;
+   public Text playerScoreText;
+   public Text computerScoreText;
+   public Ball ball;
+
+   private int _playerScore;
+   private int _computerScore;
+
+   public void PlayerScores()
+   {
+       _playerScore++;
+       this.playerScoreText.text = _playerScore.ToString();
+       ResetRound();
+   }
+
+   public void ComputerScores()
+   {
+       _computerScore++;
+       this.computerScoreText.text = _computerScore.ToString();
+       ResetRound();
+   }
+   
+   public void ResetRound()
+   {
+       this.ball.ResetPosition();
+       this.ball.AddStartingForce();
+       this.playerPaddle.ResetPosition();
+       this.computerPaddle.ResetPosition();
+   }
+
+}
